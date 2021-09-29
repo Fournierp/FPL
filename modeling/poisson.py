fe3069a50763eee07bb538ec7b17327ab28fb7a7
PF (Bot Account) <paul.ag.fournier@gmail.com>
2021-09-29 10:48:00 +0200

    Score inference

--- modeling/poisson.py
+++ modeling/poisson.py
@@ -11,21 +11,22 @@ class Poisson:
         self.games = games
 
         self.teams = np.sort(np.unique(self.games["team1"]))
+        self.league_size = len(self.teams)
 
         self.parameters = np.concatenate(
             (
-                np.repeat(1, len(self.teams)), # Attack strength
-                np.repeat(-1, len(self.teams)), # Defense strength
+                np.repeat(1, self.league_size), # Attack strength
+                np.repeat(-1, self.league_size), # Defense strength
                 [.3], # Home advantage
             )
         )
 
 
-    def score_inference(self, parameters, games, teams):
+    def score_inference(self, parameters, games):
         parameter_df = (
             pd.DataFrame()
-            .assign(attack=parameters[:len(self.teams)])
-            .assign(defence=parameters[len(self.teams) : len(self.teams) * 2])
+            .assign(attack=parameters[:self.league_size])
+            .assign(defence=parameters[self.league_size : self.league_size * 2])
             .assign(team=self.teams)
         )
 
@@ -39,8 +40,8 @@ class Poisson:
             .assign(home_adv=parameters[-1])
         )
 
-        aggregate_df["score1_infered"] = np.exp(aggregate_df["home_adv"] + aggregate_df["attack1"] + aggregate_df["defence2"])
-        aggregate_df["score2_infered"] = np.exp(aggregate_df["attack2"] + aggregate_df["defence1"])
+        aggregate_df["score1_infered"] = np.exp(aggregate_df["home_adv"] + aggregate_df["attack1"] - aggregate_df["defence2"])
+        aggregate_df["score2_infered"] = np.exp(aggregate_df["attack2"] - aggregate_df["defence1"])
 
         aggregate_df["score1_loglikelihood"] = poisson.logpmf(aggregate_df["score1"], aggregate_df["score1_infered"])
         aggregate_df["score2_loglikelihood"] = poisson.logpmf(aggregate_df["score2"], aggregate_df["score2_infered"])
@@ -51,42 +52,46 @@ class Poisson:
 
     def optimize(self):
         # Set the home rating to have a unique set of values for reproducibility
-        constraints = [{"type": "eq", "fun": lambda x: sum(x[: len(self.teams)]) - len(self.teams)}]
+        constraints = [{"type": "eq", "fun": lambda x: sum(x[: self.league_size]) - self.league_size}]
 
         # Set the maximum and minimum values the parameters of the model can take
-        bounds = [(0, 3)] * len(self.teams)
-        bounds += [(-3, 0)] * len(self.teams)
+        bounds = [(0, 3)] * self.league_size * 2
         bounds += [(0, 1)]
 
         self.solution = minimize(
             self.score_inference,
             self.parameters,
-            args=(self.games, self.teams),
+            args=self.games,
             constraints=constraints,
-            bounds=bounds
-            )
+            bounds=bounds,
+            options = {
+                "maxiter": 10,
+                "disp": False,
+            })
 
         self.parameters = self.solution["x"]
 
 
     def score_mtx(self, home_team, away_team, max_goals=8):
         # Get the corresponding model parameters
-        home_idx = np.where(teams == home_team)[0][0]
-        away_idx = np.where(teams == away_team)[0][0]
+        home_idx = np.where(self.teams == home_team)[0][0]
+        away_idx = np.where(self.teams == away_team)[0][0]
 
-        home_attack, home_defence = parameters[[home_idx, home_idx + len(teams)]]
-        away_attack, away_defence = parameters[[away_idx, away_idx + len(teams)]]
+        home = self.parameters[[home_idx, home_idx + self.league_size]]
+        away = self.parameters[[away_idx, away_idx + self.league_size]]
+        home_attack, home_defence = home[0], home[1]
+        away_attack, away_defence = away[0], away[1]
 
-        home_advantage = parameters[-1]
+        home_advantage = self.parameters[-1]
 
         # PMF
-        home_goals = np.exp(home_advantage + home_attack + away_defence)
-        away_goals = np.exp(away_attack + home_defence)
-        home_goals_vector = poisson(home_goals).pmf(np.arange(0, max_goals))
-        away_goals_vector = poisson(away_goals).pmf(np.arange(0, max_goals))
+        home_goals = np.exp(home_advantage + home_attack - away_defence)
+        away_goals = np.exp(away_attack - home_defence)
+        home_goals_pmf = poisson(home_goals).pmf(np.arange(0, max_goals))
+        away_goals_pmf = poisson(away_goals).pmf(np.arange(0, max_goals))
 
         # Aggregate probabilities
-        m = np.outer(home_goals_vector, away_goals_vector)
+        m = np.outer(home_goals_pmf, away_goals_pmf)
         return m
 
 
@@ -118,3 +123,7 @@ if __name__ == "__main__":
 
     poisson_model = Poisson(games)
     poisson_model.optimize()
+    mtx = poisson_model.score_mtx("Arsenal", "Burnley", 6)
+    print(mtx)
+    print(poisson_model.odds(mtx))
+    print(poisson_model.clean_sheet(mtx))
