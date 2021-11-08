021690f1d389ced41ff37b5e5bcf99e0b8b48bf6
PF (Bot Account) <paul.ag.fournier@gmail.com>
2021-11-08 23:33:28 +0100

    Full understat scraper

--- scraping/understat.py
+++ scraping/understat.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import re
 import codecs
+import unidecode
 
 
 class Understat:
@@ -19,6 +20,7 @@ class Understat:
         self.logger = logger
 
         self.season = season_data['season']
+        self.last_match_id = season_data['match_id']
 
         self.players = self.get_fpl_metadata()
         self.players.to_csv(self.root + '/fpl_player_ids.csv')
@@ -86,6 +88,174 @@ class Understat:
             self.save_season_data(self.root + f'/{season}-{season % 2000 + 1}', team, player)
 
 
+    def save_match_data(self, output_file, shots, player):
+        if not os.path.exists(output_file):
+            os.makedirs(output_file)
+
+        shot_df = pd.DataFrame(shots['h'])
+        shot_df = shot_df.append( pd.DataFrame(shots['a']))
+        shot_df.to_csv(output_file + '/shots.csv', index=False)
+
+        player_df = pd.DataFrame.from_dict(player['h'], orient='index')
+        player_df = player_df.append(pd.DataFrame.from_dict(player['a'], orient='index'))
+        player_df.to_csv(output_file + '/players.csv', index=False)
+
+
+    def get_match_data(self):
+        url = 'https://understat.com/match/'
+        league_name = 'EPL'
+        last_valid_match_id = 0
+
+        for i in range(self.last_match_id, self.last_match_id + 500):
+            res = requests.get(url + f'{i}')
+            parsed_html = BeautifulSoup(res.text, 'html.parser')         
+            # Get Game description
+            desc = parsed_html.findAll("ul", class_="breadcrumb")
+            # Confirm the game is in EPL league
+            for league in desc:
+                if league.find(text=re.compile(league_name)):
+                    self.logger.info(f"Downloading Match: {i} Data")
+                    # Extract the match's data
+                    scripts = parsed_html.findAll('script')
+                    shots = {}
+                    player = {}
+                    for script in scripts:
+                        for c in script.contents:
+                            split_data = c.split('=')
+                            var_name = split_data[0].strip()
+                            if var_name == 'var shotsData':
+                                content = re.findall(r'JSON\.parse\(\'(.*)\'\)', split_data[1])
+                                decoded_content = codecs.escape_decode(content[0], "hex")[0].decode('utf-8')
+                                shots = json.loads(decoded_content)
+                            elif var_name == 'var rostersData':
+                                content = re.findall(r'JSON\.parse\(\'(.*)\'\)', split_data[1])
+                                decoded_content = codecs.escape_decode(content[0], "hex")[0].decode('utf-8')
+                                player = json.loads(decoded_content)
+
+                    team_home = parsed_html.findAll("div", class_="roster roster-home")
+                    for team in team_home:
+                        team_h = team.text.strip().partition('\n')[0]
+                    
+                    team_away = parsed_html.findAll("div", class_="roster roster-away")
+                    for team in team_away:
+                        team_a = team.text.strip().partition('\n')[0]
+                        
+                    self.save_match_data(self.root + f'/matches/{league.text.strip()[-11:]}_{team_h}_vs_{team_a}', shots, player)
+
+                if (
+                    league.find(text=re.compile(league_name)) or
+                    league.find(text=re.compile('La liga')) or
+                    league.find(text=re.compile('Bundesliga')) or
+                    league.find(text=re.compile('Serie A')) or
+                    league.find(text=re.compile('RFPL')) or
+                    league.find(text=re.compile('Ligue 1'))
+                    ):
+                    # Record url was scraped
+                    last_valid_match_id = i
+
+        with open('info.json') as f:
+            season_data = json.load(f)
+
+        with open('info.json', 'w') as f:
+            season_data['match_id'] = last_valid_match_id
+            json.dump(season_data, f)
+
+
+    def _lambda_req(self, row):
+        name = row['name']
+        name = unidecode.unidecode(name)
+        print(name)
+
+        url = f'https://understat.com/main/getPlayersName/{name}'
+        res = requests.get(url.format(name=name))
+        
+        if res.status_code == 200:
+            players = json.loads(res.text)['response']['players']
+
+            if len(players) == 1:
+                return players[0]['id']
+        else:
+            name = row['second_name']
+            name = unidecode.unidecode(name)
+            res = requests.get(url.format(name=name))
+
+            if res.status_code == 200:
+                players = json.loads(res.text)['response']['players']
+
+                if len(players) == 1:
+                    return [player['id'] for player in players]
+
+            return -1
+
+
+    def get_fpl_to_understat_ids(self):
+        '''Buggy for complex, long names.'''
+        self.players['id'] = self.players.apply(lambda row : self._lambda_req(row), axis = 1)
+        self.players.to_csv(self.root + '/understat_ids.csv')
+
+
+    def save_player_data(self, output_file, groups, shots, matches):
+        if not os.path.exists(output_file):
+            os.makedirs(output_file)
+
+        seasons_df = pd.DataFrame.from_dict(groups['season'])
+        seasons_df.to_csv(output_file + '/seasons.csv', index=False)
+
+        shot_df = pd.DataFrame(shots)
+        shot_df.to_csv(output_file + '/shots.csv', index=False)
+
+        matches_df = pd.DataFrame.from_records(matches)
+        matches_df.to_csv(output_file + '/matches.csv', index=False)
+
+
+    def get_player_data(self, history=False):
+        if history:
+            seasons = [2014, 2015, 2016, 2017, 2018, 2019, 2020]
+        else :
+            seasons = [self.season]
+
+        url = 'https://understat.com/player/'
+
+        for season in seasons:
+            if not os.path.exists(f'data/understat/{season}/'):
+                os.makedirs(f'data/understat/{season}/')
+
+            df = pd.read_csv(f'data/understat/{season}/players.csv')
+            player_ids = df['id']
+
+            for idx in player_ids:
+                self.logger.info(f"Downloading Player: {idx} Data")
+                res = requests.get(url + f'{idx}')
+                parsed_html = BeautifulSoup(res.text, 'html.parser')         
+                # Extract the match's data
+                scripts = parsed_html.findAll('script')
+                groups = {}
+                shots = {}
+                matches = {}
+                # Get Player name
+                desc = parsed_html.find("ul", class_="breadcrumb")
+                name = list(desc.stripped_strings)[-1]
+
+                for script in scripts:
+                    for c in script.contents:
+                        split_data = c.split('=')
+                        var_name = split_data[0].strip()
+                        if var_name == 'var groupsData':
+                            content = re.findall(r'JSON\.parse\(\'(.*)\'\)', split_data[1])
+                            decoded_content = codecs.escape_decode(content[0], "hex")[0].decode('utf-8')
+                            groups = json.loads(decoded_content)
+                        elif var_name == 'var shotsData':
+                            content = re.findall(r'JSON\.parse\(\'(.*)\'\)', split_data[1])
+                            decoded_content = codecs.escape_decode(content[0], "hex")[0].decode('utf-8')
+                            shots = json.loads(decoded_content)
+                        elif var_name == 'var matchesData':
+                            content = re.findall(r'JSON\.parse\(\'(.*)\'\)', split_data[1])
+                            decoded_content = codecs.escape_decode(content[0], "hex")[0].decode('utf-8')
+                            matches = json.loads(decoded_content)
+
+                self.save_player_data(self.root + f'/players/{name}', groups, shots, matches)
+
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
     logger: logging.Logger = logging.getLogger(__name__)
@@ -94,4 +264,8 @@ if __name__ == "__main__":
         season_data = json.load(f)
 
     understat = Understat(logger, season_data)
-    understat.get_raw_season_data()
+    # understat.get_raw_season_data()
+    understat.get_match_data()
+    understat.get_player_data(True)
+    understat.get_player_data()
+    understat.get_fpl_to_understat_ids()
