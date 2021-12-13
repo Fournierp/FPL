Corrected budget formula & added ITB value to obj function

optimization/team_planner.py
@@ -58,7 +58,7 @@ class Team_Planner:
         # Apply random noise
         self.data = randomize(seed, self.data, self.start)
 
-    def build_model(self, model_name, objective_type='decay', decay_gameweek=0.9, vicecap_decay=0.1, decay_bench=[0.1, 0.1, 0.1, 0.1], ft_val=0):
+    def build_model(self, model_name, objective_type='decay', decay_gameweek=0.9, vicecap_decay=0.1, decay_bench=[0.1, 0.1, 0.1, 0.1], ft_val=0, itb_val=0):
         # Model
         self.model = so.Model(name=f'{model_name}_model')
 
@@ -76,6 +76,7 @@ class Team_Planner:
         self.free_transfers = self.model.add_variables(self.all_gameweeks, name='ft', vartype=so.integer, lb=1, ub=2)
         self.hits = self.model.add_variables(self.gameweeks, name='hits', vartype=so.integer, lb=0, ub=15)
         self.rolling_transfers = self.model.add_variables(self.gameweeks, name='rolling', vartype=so.binary)
+        self.in_the_bank = self.model.add_variables(self.all_gameweeks, name='itb', vartype=so.continuous, lb=0)
 
         # Objective: maximize total expected points
         # Assume a % (decay_bench) chance of a player being subbed on
@@ -100,15 +101,24 @@ class Team_Planner:
                 ft_val * self.rolling_transfers[w] # Value of having 2FT
             ) for w in self.gameweeks[1:]) # Value is added to the GW when a FT is rolled so exclude the first Gw 
 
-        self.model.set_objective(- xp - ftv, name='total_xp_obj', sense='N')
+        itbv = so.expr_sum(
+            (np.power(decay_gameweek, w - self.start - 1) if objective_type == 'linear' else 1) *
+            (
+                itb_val * self.in_the_bank[w] # Value of having 2FT
+            ) for w in self.gameweeks) # Value is added to the GW when a FT is rolled so exclude the first Gw 
+
+        self.model.set_objective(- xp - ftv - itbv, name='total_xp_obj', sense='N')
 
         # Initial conditions: set team and FT depending on the team
         self.model.add_constraints((self.team[p, self.start - 1] == 1 for p in self.initial_team), name='initial_team')
         self.model.add_constraint(self.free_transfers[self.start - 1] == self.rolling_transfer + 1, name='initial_ft')
+        self.model.add_constraint(self.in_the_bank[self.start - 1] == self.bank, name='initial_itb')
 
         # Constraints
         # The cost of the squad must exceed the budget
-        self.model.add_constraints((so.expr_sum(self.team[p, w] * self.data.loc[p, 'SV'] for p in self.players) <= self.budget for w in self.all_gameweeks), name='budget')
+        sold_amount = {w: so.expr_sum(self.sell[p, w] * self.data.loc[p, 'SV'] for p in self.players) for w in self.gameweeks}
+        bought_amount = {w: so.expr_sum(self.buy[p, w] * self.data.loc[p, 'BV'] for p in self.players) for w in self.gameweeks}
+        self.model.add_constraints((self.in_the_bank[w] == self.in_the_bank[w - 1] + sold_amount[w] - bought_amount[w] for w in self.gameweeks), name='budget')
 
         # The number of players must be 11 on field, 4 on bench, 1 captain & 1 vicecaptain
         self.model.add_constraints((so.expr_sum(self.team[p, w] for p in self.players) == 15 for w in self.all_gameweeks), name='15_players')
@@ -360,6 +370,7 @@ class Team_Planner:
             self.captain, self.vicecaptain,
             self.buy, self.sell,
             self.free_transfers, self.hits,
+            self.in_the_bank,
             freehit=-1, wildcard=-1,
             bboost=-1, threexc=-1,
             nb_suboptimal=i)
@@ -407,7 +418,7 @@ if __name__ == "__main__":
 
     tp = Team_Planner(
         team_id=35868,
-        horizon=5,
+        horizon=3,
         noise=False)
 
     tp.build_model(
@@ -416,7 +427,8 @@ if __name__ == "__main__":
         decay_gameweek=0.85,
         vicecap_decay=0.1,
         decay_bench=[0.03, 0.21, 0.06, 0.002],
-        ft_val=1.5)
+        ft_val=1.5,
+        itb_val=0.05)
 
     # tp.differential_model(
     #     nb_differentials=3,
@@ -467,11 +479,11 @@ if __name__ == "__main__":
     #     hit_limit=hit_limit,
     #     two_ft_gw=two_ft_gw)
 
-    # tp.solve(
-    #     model_name="vanilla",
-    #     log=False)
-
-    tp.suboptimals(
+    tp.solve(
         model_name="vanilla",
-        iterations=3,
-        cutoff_search='first_transfer')
+        log=True)
+
+    # tp.suboptimals(
+    #     model_name="vanilla",
+    #     iterations=3,
+    #     cutoff_search='first_transfer')

optimization/utils.py
@@ -103,8 +103,8 @@ def get_ownership_data():
     return df.set_index('id')
 
 
-def pretty_print(data, start, period, team, starter, bench, captain, vicecaptain, buy, sell, free_transfers, hits, freehit=-1, wildcard=-1, bboost=-1, threexc=-1, nb_suboptimal=0):
-    df = pd.DataFrame([], columns=['GW', 'Name', 'Pos', 'Team', 'SV', 'xP', 'Start', 'Bench', 'Cap', 'Vice', 'Ownership'])
+def pretty_print(data, start, period, team, starter, bench, captain, vicecaptain, buy, sell, free_transfers, hits, in_the_bank, freehit=-1, wildcard=-1, bboost=-1, threexc=-1, nb_suboptimal=0):
+    df = pd.DataFrame([], columns=['GW', 'Name', 'Pos', 'Team', 'SV', 'xP', 'xMins', 'Start', 'Bench', 'Cap', 'Vice', 'Ownership'])
 
     for w in np.arange(start, start+period):
         print(f"GW: {w} - FT: {int(free_transfers[w].get_value())}")
@@ -115,12 +115,21 @@ def pretty_print(data, start, period, team, starter, bench, captain, vicecaptain
                 else:
                     bo = [0]
 
-                df = df.append({'GW': w, 'Name': data.loc[p]['Name'], 'Pos': data.loc[p]['Pos'], 'Team': data.loc[p]['Team'],
-                                'SV': data.loc[p]['SV'], 'xP': data.loc[p][str(w) + '_Pts'], 'Start': int(starter[p, w].get_value()),
-                                'Bench': int(np.argmax(bo)),
-                                'Cap': int(captain[p, w].get_value()), 'Vice': int(vicecaptain[p, w].get_value()),
-                                'Ownership': data.loc[p]["Top_100"]},
-                               ignore_index=True)
+                df = df.append(
+                    {
+                        'GW': w,
+                        'Name': data.loc[p]['Name'],
+                        'Pos': data.loc[p]['Pos'],
+                        'Team': data.loc[p]['Team'],
+                        'SV': data.loc[p]['SV'],
+                        'xP': data.loc[p][str(w) + '_Pts'],
+                        'xMins' : data.loc[p][str(w) + '_xMins'],
+                        'Start': int(starter[p, w].get_value()),
+                        'Bench': int(np.argmax(bo)),
+                        'Cap': int(captain[p, w].get_value()),
+                        'Vice': int(vicecaptain[p, w].get_value()),
+                        'Ownership': data.loc[p]["Top_100"]},
+                    ignore_index=True)
 
             if buy[p, w].get_value():
                 print(f"Buy: {data.loc[p, 'Name']}")
@@ -140,7 +149,7 @@ def pretty_print(data, start, period, team, starter, bench, captain, vicecaptain
             chip = " - Chip: Triple Captain"
             av = f" - Added value: {np.sum(df.loc[(df['Cap'] == 1) & (df['GW'] == w), 'xP'])}"
 
-        print(f"xPts: {np.sum(df.loc[(df['Start'] == 1) & (df['GW'] == w), 'xP']) - hits[w].get_value()*4*(0 if wildcard == w-start else 1)*(0 if freehit == w-start else 1):.2f} - Hits: {int(hits[w].get_value())*(0 if wildcard == w-start else 1)*(0 if freehit == w-start else 1)}" + chip + av)
+        print(f"xPts: {np.sum(df.loc[(df['Start'] == 1) & (df['GW'] == w), 'xP']) - hits[w].get_value()*4*(0 if wildcard == w-start else 1)*(0 if freehit == w-start else 1):.2f} - Hits: {int(hits[w].get_value())*(0 if wildcard == w-start else 1)*(0 if freehit == w-start else 1)}" + chip + av + f" - ITB: {in_the_bank[w].get_value()/10:.1f}")
         print(" ____ ")
 
     custom_order = {'G': 0, 'D': 1, 'M': 2, 'F': 3}