3c8785b382ec65b3cc5f9db703be39e6dc30b765
PF (Bot Account) <paul.ag.fournier@gmail.com>
2022-03-01 14:19:46 +0100

    Bookies odds based baseline

--- modeling/fixtures/baselines.py
+++ modeling/fixtures/baselines.py
@@ -104,7 +104,106 @@ class Baselines:
 
         return aggregate_df
 
-    def evaluate(self, games, function_name):
+    def bookies_odds(self, games, path):
+        parameter_df = (
+            pd.DataFrame()
+            .assign(team=self.teams)
+        )
+
+        aggregate_df = (
+            pd.merge(games, parameter_df, left_on='team1', right_on='team')
+            .merge(parameter_df, left_on='team2', right_on='team')
+        )
+
+        predictions_market = (
+            pd.read_csv(f'{path}data/betting/2021-22.csv')
+            .loc[:, ["HomeTeam", "AwayTeam", "B365H", "B365D", "B365A"]]
+            .rename(columns={
+                "HomeTeam": "team1",
+                "AwayTeam": "team2",
+                "B365H": "home_win_p",
+                "B365D": "draw_p",
+                "B365A": "away_win_p"})
+        )
+
+        predictions_market = predictions_market.replace({
+            'Brighton': 'Brighton and Hove Albion',
+            'Leicester': 'Leicester City',
+            'Leeds': 'Leeds United',
+            'Man City': 'Manchester City',
+            'Man United': 'Manchester United',
+            'Norwich': 'Norwich City',
+            'Tottenham': 'Tottenham Hotspur',
+            'West Ham': 'West Ham United',
+            'Wolves': 'Wolverhampton'
+        })
+
+        aggregate_df = pd.merge(
+            aggregate_df,
+            predictions_market,
+            left_on=['team1', 'team2'],
+            right_on=['team1', 'team2'])
+
+        aggregate_df['total'] = (100 / aggregate_df['home_win_p'] + 100 /
+                    aggregate_df['draw_p'] + 100 / aggregate_df['away_win_p'])
+        aggregate_df['home_win_p'] = 100 / aggregate_df['home_win_p'] / aggregate_df['total']
+        aggregate_df['away_win_p'] = 100 / aggregate_df['away_win_p'] / aggregate_df['total']
+        aggregate_df['draw_p'] = 100 / aggregate_df['draw_p'] / aggregate_df['total']
+
+        return aggregate_df
+
+    def bookies_favorite(self, games, path):
+        parameter_df = (
+            pd.DataFrame()
+            .assign(team=self.teams)
+        )
+
+        aggregate_df = (
+            pd.merge(games, parameter_df, left_on='team1', right_on='team')
+            .merge(parameter_df, left_on='team2', right_on='team')
+        )
+
+        predictions_market = (
+            pd.read_csv(f'{path}data/betting/2021-22.csv')
+            .loc[:, ["HomeTeam", "AwayTeam", "B365H", "B365D", "B365A"]]
+            .rename(columns={
+                "HomeTeam": "team1",
+                "AwayTeam": "team2",
+                "B365H": "home_win_p",
+                "B365D": "draw_p",
+                "B365A": "away_win_p"})
+        )
+
+        predictions_market = predictions_market.replace({
+            'Brighton': 'Brighton and Hove Albion',
+            'Leicester': 'Leicester City',
+            'Leeds': 'Leeds United',
+            'Man City': 'Manchester City',
+            'Man United': 'Manchester United',
+            'Norwich': 'Norwich City',
+            'Tottenham': 'Tottenham Hotspur',
+            'West Ham': 'West Ham United',
+            'Wolves': 'Wolverhampton'
+        })
+
+        aggregate_df = pd.merge(
+            aggregate_df,
+            predictions_market,
+            left_on=['team1', 'team2'],
+            right_on=['team1', 'team2'])
+
+        max_odds = np.argmax(aggregate_df[['home_win_p', 'draw_p', 'away_win_p']].values, 1)
+
+        favorites = np.zeros(aggregate_df[['home_win_p', 'draw_p', 'away_win_p']].values.shape)
+        favorites[np.arange(0, max_odds.shape[0]), max_odds] = 1
+
+        aggregate_df['home_win_p'] = favorites[:, 0]
+        aggregate_df['away_win_p'] = favorites[:, 2]
+        aggregate_df['draw_p'] = favorites[:, 1]
+
+        return aggregate_df
+
+    def evaluate(self, games, function_name, path=''):
         if function_name == "uniform":
             aggregate_df = self.uniform(games)
         if function_name == "home":
@@ -115,6 +214,10 @@ class Baselines:
             aggregate_df = self.away_bias(games)
         if function_name == "random":
             aggregate_df = self.random_odds(games)
+        if function_name == "bookies":
+            aggregate_df = self.bookies_odds(games, path)
+        if function_name == "favorite":
+            aggregate_df = self.bookies_favorite(games, path)
 
         aggregate_df["winner"] = match_outcome(aggregate_df)
 
@@ -185,5 +288,5 @@ if __name__ == "__main__":
         .sort_values("date")
     )
 
-    predictions = baselines.evaluate(season_games[season_games['event'] == 25], 'random')
+    predictions = baselines.evaluate(season_games[season_games['event'] == next_gw], 'favorite')
     print(f"{(np.mean(predictions.rps)*100):.2f}")
