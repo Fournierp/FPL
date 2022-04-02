9b6610e9f9b319dcf6d73278bf448a6faf54ef10
PF (Bot Account) <paul.ag.fournier@gmail.com>
2022-04-02 21:02:09 +0200

    Automated chip strategy

--- optimization/app.py
+++ optimization/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import awesome_streamlit as ast
 
-from src import home, vanilla, differential, select_chips
+from src import home, vanilla, differential, select_chips, automated_chips
 
 st.set_page_config(
         page_title="FPL Optimization",
@@ -13,7 +13,9 @@ ast.core.services.other.set_logging_format()
 PAGES = {
     "Home": home,
     "Vanilla": vanilla,
+    "Differential": differential,
     "Select Chips": select_chips,
+    "Automated Chips": automated_chips,
     }
 
 
