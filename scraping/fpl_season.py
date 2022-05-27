d6773814af1d3deaabaa169c41669f06859be680
Paul Fournier <paul.ag.fournier@gmail.com>
2022-05-27 08:44:30 +0200

    Change scraping range

--- scraping/fpl_season.py
+++ scraping/fpl_season.py
@@ -75,8 +75,8 @@ class FPL_Season:
 
     def sample_ranks(self):
         """Sample every rank to get season data"""
-        last = 95000
-        end = 150000
+        last = 115000
+        end = 160000
         increment = 5000
         for ranks in np.arange(last + increment, end, increment):
             self.logger.info(f"Starting to scrape top {ranks}.")
