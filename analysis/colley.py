566ef8759c19b93e8436a1dfaf359aa6548893c5
Paul Fournier <paul.ag.fournier@gmail.com>
2022-07-04 20:02:30 +0200

    Weighted draws & comments

--- analysis/colley.py
+++ analysis/colley.py
@@ -2,18 +2,27 @@ import pandas as pd
 import numpy as np
 
 class Colley:
-    """ """
+    """ Colley sport ranking """
 
-    def __init__(self, fixtures, league_table, decay=False):
+    def __init__(self, fixtures, league_table, decay=False, draw_weight=.5):
+        """
+        Args:
+            fixtures (pd.DataFrame): Games from season to used
+            league_table (pd.DataFrame): Teams present in the league
+            decay (boolean): Apply time decay
+            draw_weight (float): Weight applied to draws
+        """
         self.results = (
             fixtures[fixtures['finished'] == True]
             [['team_a', 'team_a_score', 'team_h', 'team_h_score', "kickoff_time"]])
         self.league_table = league_table
+        self.draw_weight = draw_weight
 
         self.results["date"] = pd.to_datetime(self.results["kickoff_time"])
         self.results["days_since"] = (self.results["date"].max() - self.results["date"]).dt.days
         self.results["weight"] = self._time_decay(0.005, self.results["days_since"]) if decay else 1
 
+        # Get current season results
         self.home_results = self.results.copy()
         self.home_results['res'] = self.home_results.apply(
             lambda row: self._score_to_points_at_home(row), axis=1)
@@ -25,6 +34,11 @@ class Colley:
         self.away_results = (self.away_results.loc[:, ['team_a', 'team_h', 'res', "weight"]])
 
     def _score_to_points_at_home(self, row):
+        """
+        Computes the points accumulated by the home team
+        Args:
+            row (arraw): Fixture
+        """
         if row['team_a_score'] == row['team_h_score']:
             return 1
         elif row['team_a_score'] < row['team_h_score']:
@@ -33,6 +47,11 @@ class Colley:
             return 0
 
     def _score_to_points_at_away(self, row):
+        """
+        Computes the points accumulated by the away team
+        Args:
+            row (arraw): Fixture
+        """
         if row['team_a_score'] == row['team_h_score']:
             return 1
         elif row['team_a_score'] > row['team_h_score']:
@@ -41,6 +60,11 @@ class Colley:
             return 0
 
     def _aggregate_res(self, team):
+        """
+        Computes the overall points accumulated teams
+        Args:
+            team (int): Team index
+        """
         return (
             sum(
                 (self.home_results[self.home_results['team_h'] == team]['res'] == 3) *
@@ -57,9 +81,23 @@ class Colley:
             )
 
     def _time_decay(self, xi, t):
+        """ Compute importance weight based on time elapsed
+
+        Args:
+            xi (float): Decay rate
+            t (int): Days elapsed
+
+        Returns:
+            (float): importance weight
+        """
         return np.exp(-xi * t)
 
     def rating(self):
+        """ Computes colley ratings
+
+        Returns:
+            (pd.DataFrame): League table of ratings
+        """
         lt = (self.league_table.id.map(self._aggregate_res))
         self.league_table['res'] = lt
 
@@ -71,11 +109,13 @@ class Colley:
             x.loc[i, i] = 2
 
         for _, row in self.results.iterrows():
-            x.loc[int(row['team_h']), int(row['team_h'])] += row['weight']
-            x.loc[int(row['team_h']), int(row['team_a'])] += -row['weight']
+            res = 1 if row['team_a_score'] != row['team_h_score'] else self.draw_weight
 
-            x.loc[int(row['team_a']), int(row['team_a'])] += row['weight']
-            x.loc[int(row['team_a']), int(row['team_h'])] += -row['weight']
+            x.loc[int(row['team_h']), int(row['team_h'])] += res * row['weight']
+            x.loc[int(row['team_h']), int(row['team_a'])] -= res * row['weight']
+
+            x.loc[int(row['team_a']), int(row['team_a'])] += res * row['weight']
+            x.loc[int(row['team_a']), int(row['team_h'])] -= res * row['weight']
 
         X = x.values
 
@@ -83,4 +123,4 @@ class Colley:
 
         # Solve y = Xr
         self.league_table['colley'] = np.linalg.inv(X) @ y
-        return self.league_table.sort_values('colley')
+        return self.league_table
