666b6f42d891ccee43ca1b2cf6c425b383055f56
Paul Fournier <paul.ag.fournier@gmail.com>
2022-07-05 18:43:25 +0200

    Stadium capacities

--- scraping/wikipedia.py
+++ scraping/wikipedia.py
@@ -45,3 +45,10 @@ if __name__ == "__main__":
 
     wiki = Wikipedia(logger)
     wiki.stadium_capacity()
+
+    #PL Handbook 2021-22: https://resources.premierleague.com/premierleague/document/2021/07/22/1107e7b4-9c37-4483-9cc1-292a3ed53e6f/PL_Handbook_2021_22_DIGITAL_22.07.pdf
+    #PL Handbook 2020-21: https://resources.premierleague.com/premierleague/document/2020/09/11/dc7e76c1-f78d-45a2-be4a-4c6bc33368fa/2020-21-PL-Handbook-110920.pdf
+    #PL Handbook 2019-20: https://resources.premierleague.com/premierleague/document/2020/07/24/70ec483e-7207-42cd-89d9-576e53befedd/2019-20-PL-Handbook-240720.pdf
+    #PL Handbook 2018-19: https://resources.premierleague.com/premierleague/document/2018/07/30/8944eb84-6450-4f80-8f46-64c6e2e1b929/PL_Handbook-2018-19.pdf
+    #PL Handbook 2017-18: https://premierleague-static-files.s3.amazonaws.com/premierleague/document/2017/08/11/c494a26e-b573-41e4-bcd2-daf0ca76a00d/PL_Handbook_2017-18_Digital-4-.pdf
+    #PL Handbook 2016-17: https://resources.premierleague.com/premierleague/document/2016/08/09/b81992f4-cf2a-4c5e-a9c1-155169074163/2016-17_Premier_League_Handbook.pdf
